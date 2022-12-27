73a48ede893ca20b895f41ed1e86ccee67121a80
hcesar <hcesar@gmail.com>
2022-12-27 18:29:28 +0100

    Fix

--- angola_erp_paddle/api/util.py
+++ angola_erp_paddle/api/util.py
@@ -98,6 +98,30 @@ def paddle_ocr(img: np.array, data: str,action = "OCR PLATES",tipodoctype = None
 		else:
 			#Image DAta
 			print ('DATA imagem ')
+			#OCR IMAGE
+			ocr = PaddleOCR(lang='en')
+			img_path = img
+			result = ocr.ocr(img_path, cls=False)
+			for idx in range(len(result)):
+				res = result[idx]
+				for line in res:
+					print(line)
+
+			# draw result
+			print ('Textos no file ',txts)
+			if img:
+				return txts			
+			from PIL import Image
+			result = result[0]
+			image = Image.open(img_path).convert('RGB')
+			boxes = [line[0] for line in result]
+			txts = [line[1][0] for line in result]
+			scores = [line[1][1] for line in result]
+
+			#im_show = draw_ocr(image, boxes, txts, scores, font_path='/home/frappe/frappe-bench/apps/paddleocr/doc/fonts/simfang.ttf')
+			#im_show = Image.fromarray(im_show)
+			#im_show.save('./tmp/result.jpg')
+			print ('Textos no file ',txts)
 
 	else:
 		print ('Por fazer....')
